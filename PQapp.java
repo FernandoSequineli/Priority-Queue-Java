bf6f0b5046a590c0a25e863bccead2e1925d75c5
Fernando Sequineli <56481201+FernandoSequineli@users.noreply.github.com>
2021-02-27 10:37:14 +0000

    Update PQapp.java

--- PQapp.java
+++ PQapp.java
@@ -1,21 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package weew3;
 
-/**
- *
- * @author fernando
- */
 public class PQapp {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         MyPQ pq = new MyPQ();
         pq.enqueue(2, "Broken Toe");
         pq.enqueue(10, "coma");
